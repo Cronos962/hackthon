ae5c1209cdab27e34298f92a24ae2687060f6ce3
atharvams <atharvams009@gmail.com>
2025-11-14 12:40:14 +0530

    Auth: Login functionality added!

--- backend/auth/authRouter.js
+++ backend/auth/authRouter.js
@@ -1,10 +1,52 @@
 import express from "express";
-import { validateRegistration } from "../util/validationUtil.js";
+import {
+  validateLoginCred,
+  validateRegistration,
+} from "../util/validationUtil.js";
 import connection from "../db/dbUtil.js";
 const authRouter = express.Router();
 
-authRouter.get("/login", (req, res) => {
-  res.send("auth");
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  //validation (joi)
+  const validationErrors = validateLoginCred(req.body);
+  if (validationErrors) {
+    return res.status(400).json(validationErrors);
+  }
+
+  const doesUserExistsQuery =
+    "select user_id, email, first_name, last_name from users where email = ? and password = ?;";
+
+  try {
+    const [result] = await connection.execute(doesUserExistsQuery, [
+      email,
+      password,
+    ]);
+    console.log(result);
+    if (result.length > 0) {
+      res
+        .send({
+          message: "Success!",
+          data: result,
+        })
+        .status(200);
+    } else {
+      res
+        .send({
+          message: "User doesn't exist!",
+        })
+        .status(200);
+    }
+  } catch (err) {
+    console.log(err);
+    res
+      .sendStatus(500)
+      .send({
+        message: "Error While login!",
+      })
+      .status(500);
+  }
 });
 
 authRouter.post("/register", async (req, res) => {
@@ -18,7 +60,6 @@ authRouter.post("/register", async (req, res) => {
   }
 
   //save user in db
-
   //check if user exits
   const doesUserExistsQuery = "select email from users where email = ?;";
 
